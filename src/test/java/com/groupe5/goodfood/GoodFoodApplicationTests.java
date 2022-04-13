b044f5ce2c4af48f8d1f15d54a09f3da7fd29966
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-04-13 16:34:13 +0200

    initial commit

---
+++ src/test/java/com/groupe5/goodfood/GoodFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe5.goodfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
