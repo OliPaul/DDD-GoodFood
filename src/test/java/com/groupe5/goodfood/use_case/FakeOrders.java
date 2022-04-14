da5a0a74d749d45822a6f327923e53561af5f761
sadia ingrachen <ingrachen.sadia@gmail.com>
2022-04-14 11:26:02 +0200

    fake order

--- src/test/java/com/groupe5/goodfood/use_case/FakeOrders.java
+++ src/test/java/com/groupe5/goodfood/use_case/FakeOrders.java
@@ -14,12 +14,39 @@ public class FakeOrders implements OrderRepository {
         orders = new HashMap<>();
 
         Order order1 = new Order();
-        Dish couscous = new Dish("1", "Couscous", 15.0);
+        Dish couscous_1 = new Dish("1", "Couscous", 15.0);
         order1.setId("1");
-        order1.setDish(couscous);
-        order1.setPrice(20.50);
+        order1.setDish(couscous_1);
+        order1.setPrice(17.0);
 
-        orders.put("order-1", order1);
+        orders.put("order1", order1);
+
+        Order order2 = new Order();
+        Dish gratinDauphinois = new Dish("2", "Gratin Dauphinois", 10);
+        order2.setId("2");
+        order2.setDish(gratinDauphinois);
+        order2.setPrice(11.50);
+        orders.put("order2", order2);
+
+
+        Order order3 = new Order();
+        Dish sushi = new Dish("3", "Sushi", 9.0);
+        order1.setId("3");
+        order1.setDish(sushi);
+        order1.setPrice(10.50);
+
+        orders.put("order3", order3);
+
+        Order order4 = new Order();
+        Dish couscous_2 = new Dish("4", "Couscous", 15.0);
+        order1.setId("4");
+        order1.setDish(couscous_2);
+        order1.setPrice(17.0);
+
+        orders.put("order4", order4);
+
+
+
     }
 
     @Override
