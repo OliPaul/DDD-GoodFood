31081907aaf88971b6cf227d346d75acf2870fdc
Sonia ZEMMAM <zemsonia@hotmail.com>
2022-04-14 11:08:12 +0200

    create FakeDishes

--- src/main/java/com/groupe5/goodfood/use_case/order/DishRepository.java
+++ src/main/java/com/groupe5/goodfood/use_case/order/DishRepository.java
@@ -2,6 +2,6 @@ package com.groupe5.goodfood.use_case.order;
 
 import com.groupe5.goodfood.model.Dish;
 
-public interface OrderRepository {
+public interface DishRepository {
     Dish findById(String dishId);
 }
--- src/test/java/com/groupe5/goodfood/use_case/Order.java
+++ src/test/java/com/groupe5/goodfood/use_case/Order.java
@@ -1,10 +1,15 @@
 package com.groupe5.goodfood.use_case;
 
+import com.groupe5.goodfood.use_case.order.DishRepository;
+import com.groupe5.goodfood.use_case.order.OrderRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class Order {
+
+    private DishRepository dishes;
+    private OrderRepository orders;
 
     @Test
     void find_dish_by_id_should_return_a_dish_if_exist() {
