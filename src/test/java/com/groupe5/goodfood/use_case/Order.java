22d6838ff2495c162bb293bfe93387bb847ebd58
Sonia ZEMMAM <zemsonia@hotmail.com>
2022-04-14 11:44:37 +0200

    Test Classe Order

--- src/test/java/com/groupe5/goodfood/use_case/Order.java
+++ src/test/java/com/groupe5/goodfood/use_case/Order.java
@@ -1,9 +1,13 @@
 package com.groupe5.goodfood.use_case;
 
+import com.groupe5.goodfood.model.Dish;
 import com.groupe5.goodfood.use_case.order.DishRepository;
 import com.groupe5.goodfood.use_case.order.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 public class Order {
@@ -11,8 +15,20 @@ public class Order {
     private DishRepository dishes;
     private OrderRepository orders;
 
+    @BeforeEach
+    public void init() {
+        dishes = new FakeDishes();
+        orders = new FakeOrders();
+    }
+
     @Test
     void find_dish_by_id_should_return_a_dish_if_exist() {
+        Dish dish = dishes.findById("1");
+        assertThat(dish).isInstanceOf(Dish.class);
 
     }
+
+
+
+
 }
