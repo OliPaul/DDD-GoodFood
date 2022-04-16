801d2348a50224259d8c20894ae4d1b8aa272e66
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-04-16 20:13:45 +0200

    Update application service

--- src/main/java/com/groupe5/goodfood/use_case/OrderDish.java
+++ src/main/java/com/groupe5/goodfood/use_case/OrderDish.java
@@ -22,33 +22,29 @@ public class OrderDish {
         this.payments = payments;
     }
 
-    public Order orderDish(HashMap<String, Integer> selectedDishes) throws InvalidCreditCardException, InsufficientFundsException {
+    public Order orderDish(HashMap<String, Integer> selectedDishes, List<Dish> dishList) throws InvalidCreditCardException, InsufficientFundsException {
+        CreditCard card = payments.getCreditCard();
         Order order = new Order();
         double totalAmount = 0;
-        List<Dish> dishList = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : selectedDishes.entrySet()) {
-            //Récuperer le plat par son id
-            Optional<Dish> optionalDish = dishes.findById(entry.getKey());
-            //si le plat n'existe pas on notifie l'utilisateur
-            if (optionalDish.isEmpty()) {
-                // notify user
-                break;
-            }
-            Dish dish = optionalDish.get();
-            dishList.add(dish);
-            //Vérification de stock
-            if (dish.getQuantity() < entry.getValue()) {
-                // notify user stock pas dispo
-                break;
-            }
-            //ajouter les plat à la commande
+        List<OrderedDish> orderedDishList = new ArrayList<>();
 
+        for (Map.Entry<String, Integer> entry : selectedDishes.entrySet()) {
+            Dish dish = getDish(entry);
+            if (dish == null)
+                break;
+
+            if (verifyStock(entry, dish))
+                break;
+
+            // Create ordered dish object
             OrderedDish orderDish = new OrderedDish(dish.getId(), dish.getName(), entry.getValue(), dish.getPrice());
-            order.appendDishToOrder(orderDish);
+            orderedDishList.add(orderDish);
         }
+        //ajouter les plat à la commande
+        order.appendDishesToOrder(orderedDishList);
         // calculer le total de la commande
         order.calculateTotalPrice();
-        CreditCard card = payments.getCreditCard();
+
         // Vérifier la validité de la carte de crédit
         if (!card.validCreditCard()) {
             throw new InvalidCreditCardException("Invalid Credit card ");
@@ -57,19 +53,38 @@ public class OrderDish {
         if (card.validatePayment(order)) {
             throw new InsufficientFundsException("Card balance insufficient");
         }
-        // Enregistrer la commande
-        orders.save(order);
         // mise à jour du stock
-
         for (int i = 0; i < dishList.size(); i++) {
             Dish dish = dishList.get(i);
-
-            dish.updateStock(order.getDishes().get(i));
+            for (OrderedDish orderedDish : orderedDishList){
+                if(dish.getId().equals(orderedDish.getId())) {
+                    dish.updateStock(order.getDishes().get(i));
+                }
+            }
         }
+        // Enregistrer la commande
+        orders.save(order);
         //mise à jour de la balance
         card.updateBalance(totalAmount);
         return order;
 
+    }
+
+    private boolean verifyStock(Map.Entry<String, Integer> entry, Dish dish) {
+        //Vérification de stock
+        // notify user stock pas dispo
+        return dish.getQuantity() < entry.getValue();
+    }
+
+    private Dish getDish(Map.Entry<String, Integer> entry) {
+        //Récuperer le plat par son id
+        Optional<Dish> optionalDish = dishes.findById(entry.getKey());
+        //si le plat n'existe pas on notifie l'utilisateur
+        if (optionalDish.isEmpty()) {
+            // notify user
+            return null;
+        }
+        return optionalDish.get();
     }
 
 
