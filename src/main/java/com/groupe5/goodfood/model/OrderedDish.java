bbfab3a0ea98e2fc07c80cee5a04ae2cbaed03b7
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-04-16 20:13:21 +0200

    make it as entity

--- src/main/java/com/groupe5/goodfood/model/OrderedDish.java
+++ src/main/java/com/groupe5/goodfood/model/OrderedDish.java
@@ -1,5 +1,7 @@
 package com.groupe5.goodfood.model;
 
+import java.util.Objects;
+
 public class OrderedDish {
     private String id;
     private String name;
@@ -13,6 +15,9 @@ public class OrderedDish {
         this.price = price;
     }
 
+    public String getId() {
+        return id;
+    }
 
     public double getPrice() {
         return price;
@@ -22,4 +27,16 @@ public class OrderedDish {
         return orderedQuantity;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderedDish that = (OrderedDish) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
