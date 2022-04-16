77b3cd58cd3845659e1e1a92556be57d40b8f7b0
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2022-04-17 00:13:43 +0200

    Test case for invalid credit card

--- src/test/java/com/groupe5/goodfood/use_case/OrderDishTest.java
+++ src/test/java/com/groupe5/goodfood/use_case/OrderDishTest.java
@@ -1,5 +1,6 @@
 package com.groupe5.goodfood.use_case;
 
+import com.groupe5.goodfood.model.CreditCard;
 import com.groupe5.goodfood.model.Dish;
 import com.groupe5.goodfood.model.Order;
 import org.junit.jupiter.api.BeforeEach;
@@ -33,9 +34,10 @@ public class OrderDishTest {
             put("1", 1);
         }};
         List<Dish> dishList = dishes.getAll();
+        CreditCard creditCard = payments.getCreditCard();
 
         Exception exception = assertThrows(DishNotFoundException.class, () -> {
-            orderDish(dishesToOrder, dishList);
+            orderDish(dishesToOrder, dishList, creditCard);
         });
 
         String expectedMessage = "The dish 10 doesn't exist.";
@@ -52,9 +54,10 @@ public class OrderDishTest {
             put("1", 1);
         }};
         List<Dish> dishList = dishes.getAll();
+        CreditCard creditCard = payments.getCreditCard();
 
         Exception exception = assertThrows(EmptyStockException.class, () -> {
-            orderDish(dishesToOrder, dishList);
+            orderDish(dishesToOrder, dishList, creditCard);
         });
 
         String expectedMessage = "salade is out of stock.";
@@ -64,8 +67,28 @@ public class OrderDishTest {
 
     }
 
-    private Order orderDish(HashMap<String, Integer> selectedDishes, List<Dish> dishList) throws InvalidCreditCardException, InsufficientFundsException, EmptyStockException, DishNotFoundException {
+    @Test
+    void order_dish_with_expired_credit_card_should_throw_invalid_credit_card_exception () {
+        HashMap<String, Integer> dishesToOrder = new HashMap<>() {{
+            put("2", 1);
+            put("1", 1);
+        }};
+        CreditCard creditCard = payments.getExpiredCreditCard();
+        List<Dish> dishList = dishes.getAll();
+
+        Exception exception = assertThrows(InvalidCreditCardException.class, () -> {
+            orderDish(dishesToOrder, dishList, creditCard);
+        });
+
+        String expectedMessage = "Invalid Credit card";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+
+    }
+
+    private Order orderDish(HashMap<String, Integer> selectedDishes, List<Dish> dishList, CreditCard card) throws InvalidCreditCardException, InsufficientFundsException, EmptyStockException, DishNotFoundException {
         OrderDish order = new OrderDish(orders, dishes, payments);
-        return order.orderDish(selectedDishes, dishList);
+        return order.orderDish(selectedDishes, dishList, card);
     }
 }
